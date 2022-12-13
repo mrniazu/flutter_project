d5d9dcd67608d8c4ac5bc27624dfa488a8624163
mrniazu <120482370+mrniazu@users.noreply.github.com>
2022-12-13 07:36:56 -0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -10,10 +10,11 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Generated App',
       theme: new ThemeData(
+        brightness:Brightness.dark,
         primarySwatch: Colors.blue,
-        primaryColor: const Color(0xFF2196f3),
-        accentColor: const Color(0xFF2196f3),
-        canvasColor: const Color(0xFFfafafa),
+        primaryColor: const Color(0xFF212121),
+        accentColor: const Color(0xFF64ffda),
+        canvasColor: const Color(0xFF303030),
       ),
       home: new MyHomePage(),
     );
@@ -31,27 +32,90 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget build(BuildContext context) {
       return new Scaffold(
         appBar: new AppBar(
-          title: new Text('App Name'),
+          title: new Text('Research Management'),
           ),
-        body:
-          new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new Text(
-              "Input",
-                style: new TextStyle(fontSize:50.0,
-                color: const Color(0xFF000000),
-                fontWeight: FontWeight.w200,
-                fontFamily: "Roboto"),
-              ),
+        body: new Scaffold(
+            body: new Scaffold(
+                body:
+                  new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new Text(
+                      "Supervisor",
+                        style: new TextStyle(fontSize:26.0,
+                        color: const Color(0xFF1449e8),
+                        fontWeight: FontWeight.w900,
+                        fontFamily: "Roboto"),
+                      ),
     
-              new Expanded(
-              )
-            ]
+                      new Text(
+                      "Supervisor List",
+                        style: new TextStyle(fontSize:47.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      ),
     
-          ),
+                      new TextField(
+                        style: new TextStyle(fontSize:12.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      ),
+    
+                      new Text(
+                      "1 . Dr Nadeem Akhtar",
+                        style: new TextStyle(fontSize:23.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      ),
+    
+                      new TextField(
+                        style: new TextStyle(fontSize:12.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      ),
+    
+                      new Text(
+                      "2. Muhammad Talal",
+                        style: new TextStyle(fontSize:23.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      ),
+    
+                      new TextField(
+                        style: new TextStyle(fontSize:12.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      ),
+    
+                      new Text(
+                      "3. Dr Nauman",
+                        style: new TextStyle(fontSize:23.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      ),
+    
+                      new TextField(
+                        style: new TextStyle(fontSize:12.0,
+                        color: const Color(0xFFffffff),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                      )
+                    ]
+    
+                  ),
+    
+              );,
+    
+          );,
     
       );
     }
